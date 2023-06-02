da61fc5f9266229ed4cc92a6cfdc9e98da363dea
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2023-06-02 10:03:03 +0000

    Delete convert-the-temperature.py

--- HackerRank/convert-the-temperature.py
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    vector<double> convertTemperature(double celsius) {
-        double Kelvin = celsius + 273.15;
-        double Fahrenheit = celsius * 1.80 + 32.00;
-        return {Kelvin, Fahrenheit};
-        
-    }
-};
