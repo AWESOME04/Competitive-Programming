b9a52b4e28d3513ac111f248f05c6e06a31278b9
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2023-06-02 09:45:50 +0000

    Create set-difference.py

---
+++ HackerRank/set-difference.py
@@ -0,0 +1,9 @@
+num_of_eng = int(input())
+
+roll_num_eng = set(list(map(int, input().split())))
+
+num_of_fr = int(input())
+
+roll_num_fr = set(list(map(int, input().split())))
+
+print(len(roll_num_eng.difference(roll_num_fr)))
