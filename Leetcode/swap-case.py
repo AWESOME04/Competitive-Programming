6541584503ddb06037119bb6f01fc75417c28727
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2023-06-02 09:48:46 +0000

    Delete swap-case.py

--- Leetcode/swap-case.py
+++
@@ -1,8 +0,0 @@
-def swap_case(s):
-    string = ""
-    for i in s:
-        if i.isupper() == True:
-            string+= i.lower()
-        else:
-            string+= i.upper()
-    return string
