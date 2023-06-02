15ab37d4c74d272146f0d5b95c87dbb06bba2af6
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2023-06-02 09:05:03 +0000

    Create palindrome-number.py

---
+++ Leetcode/palindrome-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+            rev = 0
+            init_n = x
+            if( x < 0):
+              return False
+            while x != 0:
+                    rev = (rev*10) +  x % 10
+                    x = x // 10
+    
+            if(rev == init_n):
+                       return True
+            return False
