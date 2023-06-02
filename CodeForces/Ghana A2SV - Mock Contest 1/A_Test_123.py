483e184747b27cf9957f6bb64acd0f1f09b7a6de
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2023-06-02 10:10:39 +0000

    Create A_Test_123.py

---
+++ CodeForces/Ghana A2SV - Mock Contest 1/A_Test_123.py
@@ -0,0 +1,8 @@
+test_phases = []
+len_test_phases = int(input())
+for _ in range(len_test_phases):
+    curr_phase = input()
+    test_phases.append(curr_phase)
+
+for value in test_phases:
+    print(value)
