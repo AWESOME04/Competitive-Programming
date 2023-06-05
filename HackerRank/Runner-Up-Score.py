f74eae4273e68fca837d3a24858254d5d7df8afa
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2023-06-05 09:07:29 +0000

    Update Runner-Up-Score.py

--- HackerRank/Runner-Up-Score.py
+++ HackerRank/Runner-Up-Score.py
@@ -1,10 +1,6 @@
 if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
-    arr2 = sorted(arr, reverse=True)
-    for i,x in enumerate(arr2):
-        if max(arr2)==arr2[i]:
-            pass
-        else:
-            print(x)
-            break
+    arr2 = list(set(arr))
+    arr2.sort()
+    print(arr2[-2])
