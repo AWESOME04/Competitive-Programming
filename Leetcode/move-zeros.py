89f8ed50a1664c81bfc2f64c667e88ae6b96cde1
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2023-06-16 19:09:42 +0000

    Create move-zeros.py

---
+++ Leetcode/move-zeros.py
@@ -0,0 +1,11 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for num in nums:
+            if num != 0:
+                pass
+            else:
+                nums.remove(num)
+                nums.append(num)
