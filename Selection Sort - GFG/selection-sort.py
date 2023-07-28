ac7dbf26b9b7942d0c894a75146fa1c2bf31f409
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2023-07-28 18:44:44 +0000

    Added solution - LeetHub

---
+++ Selection Sort - GFG/selection-sort.py
@@ -0,0 +1,27 @@
+class Solution: 
+    def selectionSort(self, arr,n):
+        #code here
+        for i in range(len(arr)):
+            min_index = i
+            for j in range(i+1, len(arr)):
+                if arr[j] < arr[min_index]:
+                    min_index = j
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+        return arr
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        Solution().selectionSort(arr, n)
+        for i in range(n):
+            print(arr[i],end=" ")
+        print()
+# } Driver Code Ends
