bc4802f1eaa73dc23c4e093d068f92b4a74e5fd0
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2023-09-05 08:46:41 +0000

    Update remove-duplicates-from-sorted-list.py

--- Leetcode/remove-duplicates-from-sorted-list.py
+++ Leetcode/remove-duplicates-from-sorted-list.py
@@ -9,8 +9,10 @@ class Solution:
         cur = head
 
         while cur:
-            while cur.next and cur.next.val == cur.val:
+            while cur.next and cur.val == cur.next.val:
                 cur.next = cur.next.next
             cur = cur.next
 
         return head
+
+        # Time Complextiy ~ O(n)
