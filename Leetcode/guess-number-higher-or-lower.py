fc0ec50871974870b580f880661b826cfda9ca1b
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2023-09-14 15:13:26 +0000

    Create guess-number-higher-or-lower.py

---
+++ Leetcode/guess-number-higher-or-lower.py
@@ -0,0 +1,20 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        l, h = 1, n
+
+        while l<=h:
+            m = l + (h-l) // 2
+            pick = guess(m)
+            if pick == -1:
+                h = m - 1
+            elif pick == 1:
+                l = m + 1
+            else:
+                return m
