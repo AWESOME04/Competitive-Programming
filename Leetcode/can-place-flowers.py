cb478476c3dff55838007d81d93bdde8aced4015
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2023-10-21 11:26:08 +0000

    Create can-place-flowers.py

---
+++ Leetcode/can-place-flowers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        flowerbed = [0] + flowerbed + [0]
+
+        if n == 0:
+            return True
+        for i in range(1, len(flowerbed)-1):
+            if flowerbed[i-1] == 0 and flowerbed[i] == 0 and flowerbed[i+1] == 0:
+                flowerbed[i] = 1
+                n -= 1
+                if n == 0:
+                    return True
+        return False
+
