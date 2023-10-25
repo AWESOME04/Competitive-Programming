78630ed254878e29280d90502e7921ccca81e4b8
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2023-10-25 10:24:04 +0000

    Create asteroid-collision.py

---
+++ Leetcode/asteroid-collision.py
@@ -0,0 +1,20 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+
+        for asteroid in asteroids:
+            while stack and asteroid < 0 and stack[-1] > 0:
+                # Collisio detected
+                if abs(asteroid) > stack[-1]:
+                    stack.pop()
+                elif abs(asteroid) == stack[-1]:
+                    stack.pop()
+                    break
+                else:
+                    break
+            else:
+                stack.append(asteroid)
+        return stack
+
+        # Time Complexity: O(N2)
+        # Space Complexity: O(N)                
