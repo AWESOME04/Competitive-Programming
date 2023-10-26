a5b3a1d141ab8227b7365b8cd4e63e1ea3dcd77e
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2023-10-26 19:11:06 +0000

    Update climbStairs.py

--- Leetcode/climbStairs.py
+++ Leetcode/climbStairs.py
@@ -1,14 +1,20 @@
 class Solution:
-    def climbStairs(self, n: int) -> int:
-        a, b = 1, 1
-        if n <= 3:
-            return n
-        else:
-            for i in range(1, n):
-                c = a + b
-                a = b
-                b = c
-            return b
+        def climbStairs(self, n: int) -> int:
+            if n == 0:
+                return 0
+            if n == 1:
+                return 1
+            if n == 2:
+                return 2
+
+            n2 = 2
+            n3 = 3 
+
+            for i in range(n-3):
+                n3, n2 = n3+n2, n3
+
+            return n3
+        
 
         # Time Complexity: O(N)
         # Space Complexity: O(1)
