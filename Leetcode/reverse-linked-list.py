c9db4e13314da66932489722a76c58a7a888aaaf
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2023-11-03 20:38:31 +0000

    Update reverse-linked-list.py

--- Leetcode/reverse-linked-list.py
+++ Leetcode/reverse-linked-list.py
@@ -18,3 +18,23 @@ class Solution:
         return prev
 
 
+# Recursive Approach
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # base case
+        if not head or not head.next:
+            return head
+
+        # recursive call
+        rev_head = self.reverseList(head.next)
+
+        head.next.next = head
+        head.next = None
+        return rev_head
+        
