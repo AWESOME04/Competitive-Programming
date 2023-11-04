3d5ea4a1548990cc7f10973cdc91ca975f8f5994
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2023-11-04 16:09:55 +0000

    Create A_Arrays.py

---
+++ CodeForces/A_Arrays.py
@@ -0,0 +1,9 @@
+x, y = map(int, input().split())
+k, m = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+if a[k - 1] < b[y - m]:
+    print("YES")
+else:
+    print("NO")
