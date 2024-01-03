df76a57fadf3ffec0ccd82ca79238e0bf1bee008
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2024-01-03 18:01:52 +0000

    Update search-a-2d-matrix.py

--- Leetcode/search-a-2d-matrix.py
+++ Leetcode/search-a-2d-matrix.py
@@ -1,15 +1,39 @@
 class Solution:
-   def searchMatrix(self, matrix, target):
-        """
-        :type matrix: List[List[int]]
-        :type target: int
-        :rtype: bool
-        """
-        count = 0
-        for item in matrix:
-            if target in item:
-                return True
-            else:
-                continue
-        return False
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # Brute force - O(m * n)
 
+        # for i in range(len(matrix)):
+        #     for j in range(len(matrix[0])):
+        #         if matrix[i][j] == target:
+        #             return True
+        # return False
+
+
+        # O(log(m * n)) using binary search
+
+        ROWS, COLS = len(matrix), len(matrix[0])
+        top, bot = 0, ROWS - 1
+        while top <= bot:
+            mid_row = (top + bot) // 2
+            if target > matrix[mid_row][-1]:
+                top = mid_row + 1
+            elif target < matrix[mid_row][0]:
+                bot = mid_row - 1 
+            else:
+                break
+        
+        if not (top <= bot):
+            return False
+        mid_row = (top + bot) // 2
+        
+        l,r = 0, COLS - 1
+        while l <= r:
+            m = (l + r) // 2
+            if target > matrix[mid_row][m]:
+                l = m + 1
+            elif target < matrix[mid_row][m]:
+                r = m - 1
+            else:
+                return True
+
+        return False
