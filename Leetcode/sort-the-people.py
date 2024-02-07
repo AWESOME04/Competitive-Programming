226723b0681bfad3bef6dd8987bb9210c5a91841
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2024-02-07 13:31:18 +0000

    Update sort-the-people.py

--- Leetcode/sort-the-people.py
+++ Leetcode/sort-the-people.py
@@ -1,5 +1,26 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        # n = len(heights)
+        # Using Bubble sort
+
+        # for i in range(n):
+        #     for j in range(n-i-1):
+        #         if heights[j] < heights[j + 1]:
+        #             heights[j], heights[j+1] = heights[j+1], heights[j]
+        #             names[j], names[j + 1] = names[j + 1], names[j]
+        # return names
+
+        # Using Selection sort
+        # for i in range(n):
+        #     min_idx = i
+
+        #     for  j in range(i+1, n):
+        #         if heights[min_idx] < heights[j]:
+        #             min_idx = j
+        #     heights[min_idx], heights[i] = heights[i], heights[min_idx]
+        #     names[min_idx], names[i] = names[i], names[min_idx]
+        # return names
+
         n = len(names)
         data = []
 
