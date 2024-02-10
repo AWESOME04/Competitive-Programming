b9e5c52d303501451f2a5815d96493273164cc1f
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2024-02-10 15:54:15 +0000

    Create check-if-an-array-is-sorted.py

---
+++ Selection Sort - GFG/check-if-an-array-is-sorted.py
@@ -0,0 +1,39 @@
+#User function Template for python3
+
+class Solution:
+    def arraySortedOrNot(self, arr, n):
+        # code here
+        if n == 1:
+            return 1
+        
+        l = 0
+        r = 1
+        sort = 1
+        while r < n:
+            if arr[l] <= arr[r]:
+                l += 1
+                r += 1
+            else:
+                sort = 0
+                break
+        return sort
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        
+        ob = Solution()
+        ans = ob.arraySortedOrNot(arr, n)
+        if ans:
+            print(1)
+        else:
+            print(0)
+        tc -= 1
+# } Driver Code Ends
