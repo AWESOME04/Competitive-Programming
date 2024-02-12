24028dc4f82fe37ef5d2d9ab577565d5243660a0
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2024-02-12 17:02:08 +0000

    Update C_Planets.py

--- CodeForces/C_Planets.py
+++ CodeForces/C_Planets.py
@@ -1,15 +1,32 @@
-from collections import Counter
+# from collections import Counter
 
+# t = int(input())
+
+# for _ in range(t):
+#     n, c = map(int, input().split())
+#     data = list(map(int, input().split()))
+
+#     minimum_cost = 0
+#     orbit_counts = Counter(data)
+
+#     for count in orbit_counts.values():
+#         minimum_cost += min(count, c)
+
+#     print(minimum_cost)
+
+from collections import defaultdict
 t = int(input())
 
 for _ in range(t):
     n, c = map(int, input().split())
-    data = list(map(int, input().split()))
+    arr = list(map(int, input().split()))
 
-    minimum_cost = 0
-    orbit_counts = Counter(data)
+    hashmap = defaultdict(int)
 
-    for count in orbit_counts.values():
-        minimum_cost += min(count, c)
-
-    print(minimum_cost)
+    res = 0
+    for i in range(len(arr)):
+        hashmap[arr[i]] += 1
+        if hashmap[arr[i]] <= c:
+            res += 1
+    
+    print(res)
