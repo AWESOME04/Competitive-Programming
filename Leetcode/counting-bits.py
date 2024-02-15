83f1399f35664e9d2d5530b09f8730d016c26d91
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2024-02-15 11:12:14 +0000

    Update counting-bits.py

--- Leetcode/counting-bits.py
+++ Leetcode/counting-bits.py
@@ -1,9 +1,9 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
         ans = []
-        sum = 0
+        bit = 0
 
         for i in range(n+1):
-            sum = bin(i).count('1')
-            ans.append(sum)
+            bit = bin(i).count('1')
+            ans.append(bit)
         return ans
