77802eb26862a15b2b8626404fc1e13b506076f5
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2024-02-22 16:47:25 +0000

    Update coin-change.py

--- Leetcode/coin-change.py
+++ Leetcode/coin-change.py
@@ -1,16 +1,27 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        def dp(memo, i):
-            if memo[i]:
-                return memo[i]
-            if i == 0:
-                return 0
-            memo[i] = float("inf")
-            for coin in coins:
-                if i - coin >= 0:
-                    memo[i] = min(memo[i], dp(memo, i-coin) + 1)
-            return memo[i]
+        # def dp(memo, i):
+        #     if memo[i]:
+        #         return memo[i]
+        #     if i == 0:
+        #         return 0
+        #     memo[i] = float("inf")
+        #     for coin in coins:
+        #         if i - coin >= 0:
+        #             memo[i] = min(memo[i], dp(memo, i-coin) + 1)
+        #     return memo[i]
 
-        memo = collections.defaultdict(int)
-        res = dp(memo, amount)
-        return res if res != float("inf") else -1
+        # memo = collections.defaultdict(int)
+        # res = dp(memo, amount)
+        # return res if res != float("inf") else -1
+
+        # Using Bottom Up Approach
+        dp = [float("inf")] * (amount+1)
+        dp[0] = 0
+        
+        for i in range(1, amount+1):
+            for coin in coins:
+                if i - coin >=0:
+                    dp[i] = min(dp[i], dp[i-coin] + 1)
+        
+        return dp[amount] if dp[amount] != float("inf") else -1
