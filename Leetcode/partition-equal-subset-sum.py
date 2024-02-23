1cd52e624757f8fa50ffb2ce3c0e3f53dce13e48
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2024-02-23 10:25:01 +0000

    Update partition-equal-subset-sum.py

--- Leetcode/partition-equal-subset-sum.py
+++ Leetcode/partition-equal-subset-sum.py
@@ -1,16 +1,32 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        if sum(nums) % 2:
-            return False
+        # Bottom Up Approach
+        # if sum(nums) % 2:
+        #     return False
 
-        dp = set()
-        dp.add(0)
-        target = sum(nums) // 2
+        # dp = set()
+        # dp.add(0)
+        # target = sum(nums) // 2
 
-        for i in range(len(nums)-1, -1, -1):
-            next_dp = set()
-            for t in dp:
-                next_dp.add(t + nums[i])
-                next_dp.add(t)
-            dp = next_dp
-        return True if target in dp else False        
+        # for i in range(len(nums)-1, -1, -1):
+        #     next_dp = set()
+        #     for t in dp:
+        #         next_dp.add(t + nums[i])
+        #         next_dp.add(t)
+        #     dp = next_dp
+        # return True if target in dp else False
+
+        # Top Down Approach
+        n = len(nums)
+        memo = defaultdict(int)
+
+        def dp(i, target_sum):
+            if i >= n or target_sum <= 0:
+                return target_sum == 0
+            
+            state = (i, target_sum)
+            if state not in memo:
+                memo[state] = dp(i+1, target_sum-nums[i]) or dp(i+1, target_sum)
+            return memo[state]
+        
+        return sum(nums) % 2 == 0 and dp(0, sum(nums) // 2)
