2d0639245f3cd1acc8b1e4c8273b29d94c3987fa
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2024-02-29 00:42:17 +0000

    Update longest-common-prefix.py

--- Leetcode/longest-common-prefix.py
+++ Leetcode/longest-common-prefix.py
@@ -1,11 +1,37 @@
+# Trie Implementation
+class TrieNode:
+        def __init__(self):
+            self.children = {}
+            self.occurrence = 1
+
 class Solution:
-  def longestCommonPrefix(self, strs: List[str]) -> str:
-    if not strs:
-      return ''
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        root = TrieNode()
+        n = len(strs)
+        prefix = ""
 
-    for i in range(len(strs[0])):
-      for j in range(1, len(strs)):
-        if i == len(strs[j]) or strs[j][i] != strs[0][i]:
-          return strs[0][:i]
+        for s in strs:
+            cur = root
+            for i in s:
+                if i not in cur.children:
+                    cur.children[i] = TrieNode()
+                else:
+                    cur.occurrence += 1
+                
+                if cur.occurrence == n:
+                    prefix += i
+                cur = cur.children[i]
+        
+        return prefix    
 
-    return strs[0]
+# class Solution:
+#   def longestCommonPrefix(self, strs: List[str]) -> str:
+#     if not strs:
+#       return ''
+
+#     for i in range(len(strs[0])):
+#       for j in range(1, len(strs)):
+#         if i == len(strs[j]) or strs[j][i] != strs[0][i]:
+#           return strs[0][:i]
+
+#     return strs[0]
