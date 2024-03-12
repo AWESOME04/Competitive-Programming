838c3d25234ff4667edd18635fb6cd45781e4768
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2024-03-12 02:08:19 +0000

    Update search-a-2d-matrix.py

--- Leetcode/search-a-2d-matrix.py
+++ Leetcode/search-a-2d-matrix.py
@@ -11,29 +11,49 @@ class Solution:
 
         # O(log(m * n)) using binary search
 
-        ROWS, COLS = len(matrix), len(matrix[0])
-        top, bot = 0, ROWS - 1
-        while top <= bot:
-            mid_row = (top + bot) // 2
-            if target > matrix[mid_row][-1]:
-                top = mid_row + 1
-            elif target < matrix[mid_row][0]:
-                bot = mid_row - 1 
-            else:
-                break
+        # ROWS, COLS = len(matrix), len(matrix[0])
+        # top, bot = 0, ROWS - 1
+        # while top <= bot:
+        #     mid_row = (top + bot) // 2
+        #     if target > matrix[mid_row][-1]:
+        #         top = mid_row + 1
+        #     elif target < matrix[mid_row][0]:
+        #         bot = mid_row - 1 
+        #     else:
+        #         break
         
-        if not (top <= bot):
-            return False
-        mid_row = (top + bot) // 2
+        # if not (top <= bot):
+        #     return False
+        # mid_row = (top + bot) // 2
         
-        l,r = 0, COLS - 1
-        while l <= r:
-            m = (l + r) // 2
-            if target > matrix[mid_row][m]:
-                l = m + 1
-            elif target < matrix[mid_row][m]:
-                r = m - 1
-            else:
-                return True
+        # l,r = 0, COLS - 1
+        # while l <= r:
+        #     m = (l + r) // 2
+        #     if target > matrix[mid_row][m]:
+        #         l = m + 1
+        #     elif target < matrix[mid_row][m]:
+        #         r = m - 1
+        #     else:
+        #         return True
 
+        # return False
+
+        # More efficient Approach - also O(log(m * n))
+        if not matrix:
+            return False
+
+        m, n = len(matrix), len(matrix[0])
+        left, right = 0, m * n - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            mid_row, mid_col = divmod(mid, n)
+
+            if matrix[mid_row][mid_col] == target:
+                return True
+            elif matrix[mid_row][mid_col] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
         return False
+        
