b620ba16616b6037f5d02434056cb952707d843d
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2024-03-27 11:22:10 +0000

    Create array-manipulation.py

---
+++ HackerRank/array-manipulation.py
@@ -0,0 +1,54 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'arrayManipulation' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+def arrayManipulation(n, queries):
+    # Write your code here
+    diff = [0 for _ in range(0, n+1)]
+    
+    
+    for val in queries:
+        a, b, k = val
+        diff[a-1] += k
+        diff[b] -= k
+    
+    tot, maxx = 0, 0
+    for i in range(len(diff)):
+        tot += diff[i]
+        maxx = max(maxx, tot)
+    
+    return maxx
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
