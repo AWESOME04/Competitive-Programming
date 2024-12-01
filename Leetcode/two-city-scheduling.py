632214a327831f84d8a49d96f2eb7ea0b424ac80
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2024-12-01 22:38:56 +0000

    Update two-city-scheduling.py

--- Leetcode/two-city-scheduling.py
+++ Leetcode/two-city-scheduling.py
@@ -1,15 +1,15 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        """
-        - Sort the costs based on the absolute difference
-        - The smaller the cost, the higher the priority using Greedy
-        - 1st half - City A
-        - 2nd half - City B
-        """
-        costs.sort(key=lambda x: x[0] - x[1])
-        res = 0 
-        n = len(costs) // 2
+        diff = []
+        for c1, c2 in costs:
+            diff.append([c2 - c1, c1, c2])
 
-        for i in range(n):
-            res += costs[i][0] + costs[i+n][1]
+        res = 0
+        diff.sort()
+        for i in range(len(diff)):
+            if i < len(diff) // 2:
+                res += diff[i][2]
+            else:
+                res += diff[i][1]
+            
         return res
